1a338934c5dbb8c5418b473abd6ce979ac33cde8
PACFWL <pablowholock@gmail.com>
2024-09-12 22:31:16 -0300

    ok

--- models/Customer.js
+++ models/Customer.js
@@ -2,9 +2,11 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const Customer = sequelize.define('Customer', {
-  name: { type: DataTypes.STRING, allowNull: false },
-  address: { type: DataTypes.STRING },
-  neighborhood: { type: DataTypes.STRING }
+  nome: { type: DataTypes.STRING, allowNull: false },
+  telefone: { type: DataTypes.STRING },
+  origem: { type: DataTypes.STRING },
+  dataDoContacto: { type: DataTypes.DATE },
+  observacao: { type: DataTypes.TEXT }
 });
 
 module.exports = Customer;
--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -4,47 +4,44 @@ const Customer = require('../models/Customer');
 
 
 router.get('/', async (req, res) => {
-  const customers = await Customer.findAll();
-  res.render('customers/index', { customers });
-});
+    const customers = await Customer.findAll();
+    res.render('customers/index', { customers });
+  });
 
 
 router.get('/add', (req, res) => res.render('customers/add'));
 
 
 router.get('/view/:id', async (req, res) => {
-  console.log("View customer with ID: ", req.params.id);
-  const customer = await Customer.findByPk(req.params.id);
-  console.log("Customer details: ", customer);
-  res.render('customers/view', { customer });
-});
+    const customer = await Customer.findByPk(req.params.id);
+    res.render('customers/view', { customer });
+  });
 
 
 
-router.get('/edit/:id', async (req, res) => {
-  const customer = await Customer.findByPk(req.params.id);
-  res.render('customers/edit', { customer });
-});
-
+  router.get('/edit/:id', async (req, res) => {
+    const customer = await Customer.findByPk(req.params.id);
+    res.render('customers/edit', { customer });
+  });
 
 
 router.post('/add', async (req, res) => {
-  const { name, address, neighborhood } = req.body;
-  await Customer.create({ name, address, neighborhood });
-  res.redirect('/customers');
-});
+    const { nome, telefone, origem, dataDoContacto, observacao } = req.body;
+    await Customer.create({ nome, telefone, origem, dataDoContacto, observacao });
+    res.redirect('/customers');
+  });
 
 
-router.post('/edit/:id', async (req, res) => {
-  const { name, address, neighborhood } = req.body;
-  await Customer.update({ name, address, neighborhood }, { where: { id: req.params.id } });
-  res.redirect('/customers');
-});
+  router.post('/edit/:id', async (req, res) => {
+    const { nome, telefone, origem, dataDoContacto, observacao } = req.body;
+    await Customer.update({ nome, telefone, origem, dataDoContacto, observacao }, { where: { id: req.params.id } });
+    res.redirect('/customers');
+  });
 
 
-router.get('/delete/:id', async (req, res) => {
-  await Customer.destroy({ where: { id: req.params.id } });
-  res.redirect('/customers');
-});
+  router.get('/delete/:id', async (req, res) => {
+    await Customer.destroy({ where: { id: req.params.id } });
+    res.redirect('/customers');
+  });
 
 module.exports = router;
